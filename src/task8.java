01ec5b6307889fe83a97e7e23b6d854c9da8a613
Student <student@gmail.com>
2026-09-24 10:00:01 +0600

    task8

---
+++ src/task8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class task8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int v = input.nextInt();
+
+
+
+
+        System.out.println((v / 10) % 10);
+
+    }
+}
