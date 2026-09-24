abeacb4cb0779a54d939c0e4fc360b8567ea5c80
Student <student@gmail.com>
2026-09-24 10:21:00 +0600

    task17

---
+++ src/task17.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class task17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println((m+n-1)/n);
+
+
+
+    }
+}
