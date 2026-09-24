b92632f80e4e6eaf355eb091318278d7642eae6e
Student <student@gmail.com>
2026-09-24 09:57:42 +0600

    task6

---
+++ src/task6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class task6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int v = input.nextInt();
+
+
+
+
+        System.out.println(v % 10);
+
+    }
+}
